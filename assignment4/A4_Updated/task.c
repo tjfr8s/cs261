4c9e63594b0c30a280a05e12c5e9d6b61379f907
tyler <tjfr8s@gmail.com>
2018-11-09 13:44:47 -0800

    assignment 3 and assignment 4

---
+++ assignment4/A4_Updated/task.c
@@ -0,0 +1,67 @@
+/*
+ * CS 261 Assignment 4
+ * Name: 
+ * Date: 
+ */
+
+#include "task.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * Creates a new task with the given priority and name.
+ * @param priority
+ * @param name
+ * @return  The new task.
+ */
+Task* taskNew(int priority, char* name)
+{
+    // FIXME: implement
+    Task* newTask = malloc(sizeof(Task));
+    newTask->priority = priority;
+    strcpy(newTask->name, name);
+    return newTask;
+}
+
+/**
+ * Frees a dynamically allocated task.
+ * @param task
+ */
+void taskDelete(Task* task)
+{
+    free(task);
+}
+
+/**
+ * Casts left and right to Task pointers and returns
+ * -1 if left's priority < right's priority,
+ *  1 if left's priority > right's priority,
+ *  0 if left's priority == right's priority.
+ * @param left  Task pointer.
+ * @param right  Task pointer.
+ * @return 
+ */
+int taskCompare(void* left, void* right)
+{
+    // FIXME: implement
+    if(((Task*)left)->priority > ((Task*)right)->priority){
+        return 1;
+    }
+    else if(((Task*)left)->priority < ((Task*)right)->priority){
+        return -1;
+    }
+    else{
+        return 0;
+    }
+}
+
+/**
+ * Prints a task as a (priority, name) pair.
+ * @param value  Task pointer.
+ */
+void taskPrint(void* value)
+{
+    Task* task = (Task*)value;
+    printf("(%d, %s)", task->priority, task->name);
+}
